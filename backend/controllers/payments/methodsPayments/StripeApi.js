fe8021e9f67154cfcde63ee769e5cefc16a248f3
carlos676767 <carlitobr780@yahoo.com>
2024-11-27 17:53:38 -0800

    ok

---
+++ backend/controllers/payments/methodsPayments/StripeApi.js
@@ -0,0 +1,55 @@
+class StripeApi {
+  static #stripeApi = require("stripe")(process.env.SECRET_KEY_STRIPE);
+
+  static #informacoesPagamento(valor, itens) {
+    const valorParaMultiplicarEmCentavos = 100;
+    const valorEmCentavos = valor * valorParaMultiplicarEmCentavos;
+    const price_data = {
+      price_data: {
+        currency: "brl",
+        product_data: { name: `${itens}` },
+        unit_amount: valorEmCentavos,
+      },
+      quantity: 1,
+    };
+
+    return price_data;
+  }
+
+  static configsRedirecionamentoEpagamneto() {
+    return {
+      mode: "payment",
+      success_url: "https://www.exemplo.com",
+      cancel_url: "https://www.exemplo.com",
+    };
+  }
+
+  static async gerarPagamento(valor, itens) {
+    try {
+      const { url } = await StripeApi.#stripeApi.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [this.#informacoesPagamento(valor, itens)],
+        ...this.configsRedirecionamentoEpagamneto(),
+      });
+
+      return url;
+    } catch (error) {
+      throw new Error("error ao criar pagamento.");
+    }
+  }
+
+
+
+
+  static webWhook(req, res){
+    const typeNotification = req.body.type
+    if (typeNotification === `charge.succeeded`) {
+        return
+    }
+
+    
+  }
+}
+
+
+module.exports = StripeApi;
