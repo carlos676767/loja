f796a63eb1363924b23574c905439c40188fd2e2
carlos676767 <carlitobr780@yahoo.com>
2024-12-10 10:36:23 -0800

    agora envia email quando o usuario cria uma conta.

--- backend/controllers/RegistrationUserController.js
+++ backend/controllers/RegistrationUserController.js
@@ -1,39 +1,42 @@
-
-"use strict"
+"use strict";
 
 class JsonWebToken {
-  static jwt = require(`jsonwebtoken`)
-  static tokenJwt(id){
+  static jwt = require(`jsonwebtoken`);
+  static tokenJwt(id) {
     const config = {
-        id
-    }
-    return this.jwt.sign(config, process.env.SECRET_KEY_JWT, {expiresIn: `2h`})
+      id,
+    };
+    return this.jwt.sign(config, process.env.SECRET_KEY_JWT, {
+      expiresIn: `2h`,
+    });
   }
 }
 
-
 class DatabaseService {
-  static Sql = require("../db/database")
-  static async insertUser(email, senha, res){
-    const db = await this.Sql.db()
+  static Sql = require("../db/database");
+  static async insertUser(email, senha, res) {
+    const db = await this.Sql.db();
     try {
-         await db.exec(`BEGIN TRANSACTION`)
+      await db.exec(`BEGIN TRANSACTION`);
 
-         const {lastID} =  await db.run(`INSERT INTO USER(email, senha) VALUES(?, ?)`, [email,senha])
+      const { lastID } = await db.run(
+        `INSERT INTO USER(email, senha) VALUES(?, ?)`,
+        [email, senha]
+      );
 
-         await db.exec(`COMMIT`)
+      await db.exec(`COMMIT`);
 
-         res.status(200).send({jwt: JsonWebToken.tokenJwt(lastID), register: true})
-
+      res
+        .status(200)
+        .send({ jwt: JsonWebToken.tokenJwt(lastID), register: true });
     } catch (error) {
-        await db.exec(`ROLLBACK`)
-        throw new Error(error.message);
-    }finally{
-        await db.close()
+      await db.exec(`ROLLBACK`);
+      throw new Error(error.message);
+    } finally {
+      await db.close();
     }
-  } 
+  }
 }
-
 
 class GetUser {
   static cache = require(`../cache/cacheData`);
@@ -41,55 +44,71 @@ class GetUser {
     const getUser = this.cache.get(`dadosUser`);
 
     if (getUser == undefined) {
-      throw new Error("The time to check your email has passed, make a new request or enter an invalid code, check and try again" );
+      throw new Error(
+        "The time to check your email has passed, make a new request or enter an invalid code, check and try again"
+      );
     }
 
-    const { email, senha, code } = getUser
+    const { email, senha, code } = getUser;
 
-    
-    return {email,senha, code}
+    return { email, senha, code };
   }
-
 }
 
 class ValidacoesUser {
-  static verifyCode(codigo){
+  static verifyCode(codigo) {
     if (!codigo) {
-        throw new Error("enter the code");
+      throw new Error("enter the code");
     }
   }
 
-
-
-  static verifyCodeInConfirm(codigoUserBody, codeInCache){
+  static verifyCodeInConfirm(codigoUserBody, codeInCache) {
     if (codigoUserBody != codeInCache) {
-        throw new Error("the code provided is not the same in our database");
+      throw new Error("the code provided is not the same in our database");
     }
 
-    return true
+    return true;
   }
 }
 
-class RegistrationUserController extends ValidacoesUser {
+class ConfigEmailRegister {
+  static Email = require(`../utils/email/email`);
+  static async sendEmail(user) {
+    const configEmail = {
+      text: `Olá, Estrela!  
+
+Parabéns por dar o primeiro passo e criar sua 
+conta no csdev assinaturas
+ Estamos muito felizes por tê-lo conosco nesta jornada.`,
+      userEmail: user,
+      title: `Bem-vindo ao csdev assinatura! Conta criada com sucesso!  `,
+    };
+
+    const {text, title, userEmail} = configEmail
+    await ConfigEmailRegister.Email.sendEmail(text, userEmail, title)
+  }
+}
+
+class RegistrationUserController extends ValidacoesUser  {
   static async router(req, res) {
     try {
-        const {codigo} = req.body
-        ValidacoesUser.verifyCode(codigo)
+      const { codigo } = req.body;
+      ValidacoesUser.verifyCode(codigo);
 
-        const {code, senha,  email} = GetUser.verifyCache()
+      const { code, senha, email } = GetUser.verifyCache();
 
-        const codeConfirm = ValidacoesUser.verifyCodeInConfirm(codigo, code)
+      const codeConfirm = ValidacoesUser.verifyCodeInConfirm(codigo, code);
 
-        if (codeConfirm) {
-        return  await DatabaseService.insertUser(email, senha, res)
-        }
+      if (codeConfirm) {
+        await DatabaseService.insertUser(email, senha, res);
+        await ConfigEmailRegister.sendEmail(email)
+      }
 
-        throw new Error("Error registering user, try again.");
-        
+      throw new Error("Error registering user, try again.");
     } catch (error) {
-        res.status(400).send({err: error.message})
+      res.status(400).send({ err: error.message });
     }
   }
-} 
+}
 
 module.exports = RegistrationUserController;
